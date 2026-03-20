f45189e9f63fad54b4d24eb84204cf3bb0ea9b5a
Lana Ramsak <ramsaklana@gmail.com>
2026-03-20 13:20:09 +0100

    Adapted the models to all use the same set of images

--- EfficientNet.py
+++ EfficientNet.py
@@ -4,10 +4,7 @@ import torch.nn as nn
 from sklearn.model_selection import train_test_split
 from torchvision import models
 
-from import_data import get_loader
-
-
-loader = get_loader()
+from import_data import DEFAULT_PATHS_SMALL, DEFAULT_Y_SMALL, get_loader
 
 
 # EFFICIENTNET-B0 FEATURE EXTRACTION
@@ -18,18 +15,28 @@ efficientnet.eval()
 device = "cuda" if torch.cuda.is_available() else "cpu"
 efficientnet.to(device)
 
-all_feats = []
-all_y = []
 
-with torch.no_grad():
-    for Xb, yb in loader:
-        Xb = Xb.to(device)
-        feats = efficientnet(Xb)
-        all_feats.append(feats.cpu().numpy())
-        all_y.append(yb.numpy())
+def extract_features_EfficientNet(paths=None, labels=None):
+    loader = get_loader(paths=paths, labels=labels)
+    all_feats = []
+    all_y = []
 
-X_feat_efficientnet = np.concatenate(all_feats, axis=0)
-y_np_efficientnet = np.concatenate(all_y, axis=0)
+    with torch.no_grad():
+        for Xb, yb in loader:
+            Xb = Xb.to(device)
+            feats = efficientnet(Xb)
+            all_feats.append(feats.cpu().numpy())
+            all_y.append(yb.numpy())
+
+    X_feat_efficientnet = np.concatenate(all_feats, axis=0)
+    y_np_efficientnet = np.concatenate(all_y, axis=0)
+    return X_feat_efficientnet, y_np_efficientnet
+
+
+X_feat_efficientnet, y_np_efficientnet = extract_features_EfficientNet(
+    DEFAULT_PATHS_SMALL,
+    DEFAULT_Y_SMALL
+)
 
 
 def extract_subsets_EfficientNet(
